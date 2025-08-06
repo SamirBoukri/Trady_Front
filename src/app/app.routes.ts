26208a48f02a708a0a51c37d0053256b9265e7d0
unknown <vincent.vuagnat@orange.fr>
2025-08-06 17:52:32 +0200

    added dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,16 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./features/dashboard/dashboard').then((m) => m.Dashboard),
+  },
+  {
     path: 'auth',
     loadChildren: () =>
       import('./features/auth/auth.module').then((m) => m.AuthModule),
